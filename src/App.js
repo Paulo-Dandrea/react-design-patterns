4478b2e8b9970d1a3bfd5bb1e8900aae1a8bf97c
Paulo Dandrea <37453518+Paulo-Dandrea@users.noreply.github.com>
2021-11-01 16:30:42 -0300

    feat: improve splitScreen component with childre

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import SplitScreen from "./layout/split-screen";
 import "./styles.css";
 
+const LeftHand = () => <h1 style={{ background: "crimson" }}>Esquerda</h1>;
+const RightHand = () => <h1 style={{ background: "orange" }}>Esquerda</h1>;
+
 export default function App() {
   return (
-    <SplitScreen
-      left={() => <h1>Esquerda</h1>}
-      right={() => <h1>Direita</h1>}
-    />
+    <SplitScreen leftWeight="3">
+      <LeftHand />
+      <RightHand />
+    </SplitScreen>
   );
 }
