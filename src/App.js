c5c3f0195f162b34d3cf4415472327163dfa881b
Paulo Dandrea <37453518+Paulo-Dandrea@users.noreply.github.com>
2021-11-01 16:05:20 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import SplitScreen from "./layout/split-screen";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <SplitScreen
+      left={() => <h1>Esquerda</h1>}
+      right={() => <h1>Direita</h1>}
+    />
   );
 }
