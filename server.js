d29fe21f82f5ece5011328bb6fb254733ae4ee28
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-11-05 17:36:17 -0300

    feat: DataSource component

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 let currentUser = {
-  id: '1',
+  id: "1",
   name: "John Doe",
   age: 54,
   hairColor: "brown",
@@ -14,21 +14,21 @@ let currentUser = {
 
 let users = [
   {
-    id: '1',
+    id: "1",
     name: "John Doe",
     age: 54,
     hairColor: "brown",
     hobbies: ["swimming", "bicycling", "video games"],
   },
   {
-    id: '2',
+    id: "2",
     name: "Brenda Smith",
     age: 33,
     hairColor: "black",
     hobbies: ["golf", "mathematics"],
   },
   {
-    id: '3',
+    id: "3",
     name: "Jane Garcia",
     age: 27,
     hairColor: "blonde",
@@ -38,21 +38,21 @@ let users = [
 
 const products = [
   {
-    id: '1',
+    id: "1",
     name: "Flat-Screen TV",
     price: "$300",
     description: "Huge LCD screen, a great deal",
     rating: 4.5,
   },
   {
-    id: '2',
+    id: "2",
     name: "Basketball",
     price: "$10",
     description: "Just like the pros use",
     rating: 3.8,
   },
   {
-    id: '3',
+    id: "3",
     name: "Running Shoes",
     price: "$120",
     description: "State-of-the-art technology for optimum running",
@@ -67,7 +67,7 @@ app.get("/current-user", (req, res) => {
 app.get("/users/:id", (req, res) => {
   const { id } = req.params;
 
-  const targetUser = users.find((user) => user.id === id) || 'user not found'
+  const targetUser = users.find((user) => user.id === id) || "user not found";
   res.json(targetUser);
 });
 
--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ import { UserInfo } from "./business-components/UserInfo";
 import { ProductInfo } from "./business-components/ProductInfo";
 import { CurrentUserLoader } from "./containers/CurrentUserLoader";
 import { UserLoader } from "./containers/UserLoader";
+import { ResourceLoader } from "./containers/ResouceLoader";
+import { DataSource } from "./containers/DataSource";
+import axios from "axios";
+
+const baseURL = "http://localhost:8080";
+
+const getServerData = async (url) => {
+  const response = await axios.get(url, { baseURL });
+  return response.data;
+};
+
+const getLocalStorage = async (key) => {
+  const item = localStorage.getItem(key);
+  return JSON.parse(item);
+};
+
+const Text = ({ text }) => <div>{text}</div>;
 
 export default function App() {
   return (
@@ -18,13 +35,44 @@ export default function App() {
           <UserInfo />
         </UserLoader>
       </Modal>
-      
+
       <Modal openModalButtonText={"Current User"}>
         <CurrentUserLoader>
-          Current User Loader:
           <UserInfo />
         </CurrentUserLoader>
       </Modal>
+      {/* Doing the same thing with ResourceLoader, a dynamic version of the loader */}
+      <Modal openModalButtonText={"Current user with ResourceLoader"}>
+        <ResourceLoader resourceUrl="/current-user" resourceName="user">
+          <UserInfo />
+        </ResourceLoader>
+      </Modal>
+
+      <Modal openModalButtonText={"get Product with ResourceLoader"}>
+        <ResourceLoader resourceUrl="/products/2" resourceName="product">
+          <ProductInfo />
+        </ResourceLoader>
+      </Modal>
+
+      <Modal openModalButtonText={"get Product from Data Source"}>
+        <DataSource
+          getDataFunc={() => getServerData("/products/2")}
+          resourceName="product"
+        >
+          <ProductInfo />
+        </DataSource>
+      </Modal>
+
+
+      <Modal openModalButtonText={"get message from localStorage"}>
+        <DataSource
+          getDataFunc={() => getLocalStorage("message")}
+          resourceName="text"
+        >
+          <Text />
+        </DataSource>
+      </Modal>
+
 
       <SplitScreen leftWeight="2">
         <Modal openModalButtonText={"Open product list summary"}>
