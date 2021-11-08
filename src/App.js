e76d78c1a629975ccdbcfd105504114e486055a6
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-11-08 18:33:55 -0300

    refactor: remove unused import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-import { useState } from "react";
-// Data
-import { people, products } from "./data";
-// Layout
-import { RegularList } from "./layout/components/RegularList";
-import SplitScreen from "./layout/split-screen";
-import { UncontrolledModal } from "./layout/components/UncontrolledModal";
-// Business Components
-import { UserInfo } from "./business-components/UserInfo";
-import { ProductInfo } from "./business-components/ProductInfo";
-import { CurrentUserLoader } from "./containers/CurrentUserLoader";
-import { UserLoader } from "./containers/UserLoader";
-import { ResourceLoader } from "./containers/ResouceLoader";
-import { DataSource } from "./containers/DataSource";
-import axios from "axios";
-import { UncontrolledForm } from "./layout/components/UncontrolledForm";
-import { ControlledForm } from "./layout/components/ControlledForm";
-import { ControlledModal } from "./layout/components/ControlledModal";
-import { UncontrolledOnboardingFlow } from "./layout/components/UncontrolledOnboardingFlow";
-import { ControlledOnboardingFlow } from "./layout/components/ControlledOnboardingFlow";
 import { Link } from "react-router-dom";
 
 const baseURL = "http://localhost:8080";
